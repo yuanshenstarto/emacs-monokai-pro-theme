52f0114a938e8e331d64dcbfa8696a369fda8334
yuanshenstarto <yuanshenstarto@gmail.com>
2025-12-27 03:39:34 +0800

    feat: update light-sun

--- monokai-pro-light-sun-theme.el
+++ monokai-pro-light-sun-theme.el
@@ -7,35 +7,35 @@
 (defvar monokai-pro-light-sun-theme-colors
   '(;; Background and foreground colors
     :bg     "#f8efe7"
-    :bg+1   "#dfc9b6"
-    :bg+2   "#b38e6e"
-    :fg-4   "#a097a4"
-    :fg-3   "#827a85"
+    :bg+1   "#cbb19a"
+    :bg+2   "#a77f5c"
+    :fg-4   "#b1a9b5"
+    :fg-3   "#8e8791"
     :fg-2   "#665e69"
     :fg-1   "#423845"
     :fg     "#2c232e"
 
-    ;; General colors
+    ;; General colors (ANSI color of light-sun in iTerm)
     :white  "#2c232e"
     :red    "#ce4770"
-    :orange "#ff9b5e"
+    :orange "#e25016"
     :yellow "#b16803"
     :green  "#218871"
     :blue   "#d4572b"
-    :purple "#6851a2"
+    :purple "#6851a2" ;; magenta
     :pink   "#ff657a"
-    :black  "#f8efe7"
 
     ;; Colors from the original Monokai colorschemes. Some of these are used
     ;; rarely as highlight colors. They should be avoided if possible.
-    :orig-red     "#f92672"
+    ;;; set as the light ANSI color of light-sun in iTerm
+    :orig-red     "#ce4770"
     :orig-orange  "#fd971f"
-    :orig-yellow  "#e6db74"
-    :orig-green   "#a6e22e"
-    :orig-cyan    "#a1efe4"
-    :orig-blue    "#66d9ef"
-    :orig-violet  "#ae81ff"
-    :orig-magenta "#fd5ff0"))
+    :orig-yellow  "#b16803"
+    :orig-green   "#218871"
+    :orig-cyan    "#2473b6"
+    :orig-blue    "#d4572b"
+    :orig-violet  "#242eb6" ;; customized
+    :orig-magenta "#6851a2"))
 
 (deftheme monokai-pro-light-sun)
 (monokai-pro-theme-define 'monokai-pro-light-sun monokai-pro-light-sun-theme-colors)
